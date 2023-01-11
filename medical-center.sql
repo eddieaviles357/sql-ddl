015b6d666bca30e483932b3faf854ede46462570
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-01-10 20:48:39 -0800

    formated text

--- medical-center.sql
+++ medical-center.sql
@@ -8,31 +8,17 @@ CREATE DATABASE medical;
 
 CREATE TABLE medical_center (
     id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
-    phone_number VARCHAR(13) UNIQUE NOT NULL
+    name TEXT NOT NULL UNIQUE,
+    phone_number VARCHAR(13) NOT NULL UNIQUE
 );
 
-INSERT INTO medical_center (name, phone_number)
-VALUES 
-('Best Urgent Care', '(949)888-1111'),
-('Kids Care', '(949)305-1111'),
-('Genki Care', '(714)935-2222');
 
 CREATE TABLE diseases (
     id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     curable BOOLEAN NOT NULL
 );
 
-INSERT INTO diseases (name, curable)
-VALUES
-('Parkinsons', FALSE),
-('HIV', FALSE),
-('Herpes', FALSE),
-('Cold', TRUE),
-('Flu', TRUE),
-('Covid', TRUE),
-('Alzheimers', TRUE);
 
 CREATE TABLE doctors (
     id SERIAL PRIMARY KEY,
@@ -41,11 +27,6 @@ CREATE TABLE doctors (
     medical_center_id INTEGER REFERENCES medical_center ON DELETE SET NULL
 );
 
-INSERT INTO doctors (first_name, last_name, medical_center_id)
-VALUES
-('Jhon', 'Parkinson', 1),
-('Jose', 'Araiza', 2),
-('Wasim', 'Raja', 3);
 
 
 CREATE TABLE patients (
@@ -57,13 +38,6 @@ CREATE TABLE patients (
     diseases_id INTEGER REFERENCES diseases ON DELETE SET NULL
 );
 
-INSERT INTO patients (first_name, last_name, birth_date, sex, diseases_id)
-VALUES
-('Eddie', 'Aviles', '1987-07-09', 'M', 4),
-('Josh', 'Acorn', '2000-01-09', 'M', 4),
-('Mayra', 'Whale', '1980-09-15', 'F', 1),
-('Nancy', 'Santos', '1949-12-23', 'F', 5),
-('Jesse', 'Palma', '1990-12-23', 'F', NULL);
 
 CREATE TABLE doctors_patients (
     id SERIAL PRIMARY KEY,
@@ -83,3 +57,33 @@ VALUES
 (1, 5),
 (2, 5);
 
+
+INSERT INTO medical_center (name, phone_number)
+VALUES 
+('Best Urgent Care', '(949)888-1111'),
+('Kids Care', '(949)305-1111'),
+('Genki Care', '(714)935-2222');
+
+INSERT INTO diseases (name, curable)
+VALUES
+('Parkinsons', FALSE),
+('HIV', FALSE),
+('Herpes', FALSE),
+('Cold', TRUE),
+('Flu', TRUE),
+('Covid', TRUE),
+('Alzheimers', TRUE);
+
+INSERT INTO doctors (first_name, last_name, medical_center_id)
+VALUES
+('Jhon', 'Parkinson', 1),
+('Jose', 'Araiza', 2),
+('Wasim', 'Raja', 3);
+
+INSERT INTO patients (first_name, last_name, birth_date, sex, diseases_id)
+VALUES
+('Eddie', 'Aviles', '1987-07-09', 'M', 4),
+('Josh', 'Acorn', '2000-01-09', 'M', 4),
+('Mayra', 'Whale', '1980-09-15', 'F', 1),
+('Nancy', 'Santos', '1949-12-23', 'F', 5),
+('Jesse', 'Palma', '1990-12-23', 'F', NULL);
